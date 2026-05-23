76b99a9f378d0a6310194968449e30c4b8dfe78f
NoxPirate <ritiks.987654321@gmail.com>
2026-05-23 16:34:25 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -17,11 +17,11 @@ def init_db():
             print("Admin 'arman@admin.com' created.")
 
         # Create Regular User
-        if not User.query.filter_by(email="ritiks.987654321@gmail.com").first():
-            u = User(email="ritiks.987654321@gmail.com", name="Ritik", role="user")
+        if not User.query.filter_by(email="").first():
+            u = User(email="", name="Ritik", role="user")
             u.set_password("1")
             db.session.add(u)
-            print("User 'ritiks.987654321@gmail.com' created.")
+            print("User '' created.")
 
         db.session.commit()
         print("Database initialization complete.")
